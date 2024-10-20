02f61d4d231011af749bab3c953f0fed51cfa1e6
Vladimir <eurasian@list.ru>
2024-10-20 16:25:38 +0300

    First commit

---
+++ src/main/java/com/example/demo/repositories/UserRepository.java
@@ -0,0 +1,80 @@
+package com.example.demo.repositories;
+
+import com.example.demo.model.User;
+import com.example.demo.utils.UserMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+    @Repository
+    public class UserRepository {
+
+        private final JdbcTemplate jdbcTemplate;
+
+        public UserRepository(JdbcTemplate jdbcTemplate) {
+            this.jdbcTemplate = jdbcTemplate;
+        }
+
+
+
+        public User findUserById(Integer id) {
+            String sql = "SELECT * FROM userTable WHERE id=?";
+            RowMapper<User> userRowMapper = (r, i) -> {
+                User rowObject = new User();
+                rowObject.setId(r.getInt("id"));
+                rowObject.getFirstName();
+                rowObject.getLastName();
+                return rowObject;
+            };
+            return jdbcTemplate.query(sql, new Object[]{id}, new UserMapper())
+                    .stream().findFirst().orElse(null);
+        }
+
+
+        //    Получение всех пользователей из БД
+        public List<User> findAll(){
+        String sql = "SELECT * FROM userTable";
+
+        RowMapper<User> userRowMapper = (r, i) -> {
+            User rowObject = new User();
+            rowObject.setId(r.getInt("id"));
+            rowObject.setFirstName(r.getString("firstName"));
+            rowObject.setLastName(r.getString("lastName"));
+            return rowObject;
+        };
+        return jdbcTemplate.query(sql, userRowMapper);
+    }
+
+//        public List<User> findAll(){
+//            String sql = "SELECT * FROM userTable";
+//            return jdbcTemplate.query(sql, new UserMapper());
+//        };
+
+
+
+    //    добавление в БД
+    public User save(User user) {
+        String sql = "INSERT INTO userTable VALUES (NULL, ?, ?)";
+        jdbcTemplate.update(sql, user.getFirstName(), user.getLastName());
+        return user;
+    }
+
+    //    удаление из БД
+    public void deleteById (int id) {
+        String sql = "DELETE FROM userTable WHERE id=?";
+        jdbcTemplate.update(sql, id);
+    }
+
+    /* Обновление пользователя в БД */
+    public void update(User user){
+        String sql = "UPDATE userTable SET firstName=?, lastName=? WHERE id=?";
+        jdbcTemplate.update(sql, user.getFirstName(), user.getLastName(), user.getId());
+    }
+
+
+}
+
+
+
